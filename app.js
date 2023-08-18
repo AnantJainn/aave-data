63019d1bae9575f77a0b20f2c7d9fbda27aa903b
Anant Jain <ritujainanant2810@gmail.com>
2023-08-18 13:21:04 +0530

    .

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ app.use('/uploads',express.static('uploads'));
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
+app.use(express.static('public'))
 app.use((req,res,next)=>{
     res.header('Access-Control-Allow-Origin','*');
     res.header('Access-Control-Allow-Headers','*');
