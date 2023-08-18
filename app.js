f1ee102cef397d1e9c76e8aa71651d2095c82e90
Anant Jain <ritujainanant2810@gmail.com>
2023-08-18 15:38:45 +0530

    .

--- app.js
+++ app.js
@@ -1,63 +1,85 @@
 
-const express = require('express');
+// const express = require('express');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const dotenv = require('dotenv');
 const Aave = require("./api/routes/aave.js");
 
-dotenv.config();
+// dotenv.config();
 
-const app = express();
+// const app = express();
 
-// Middleware
-app.use(cors());
-app.use(morgan('dev'));
-app.use('/uploads', express.static('uploads'));
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+// // Middleware
+// app.use(cors());
+// app.use(morgan('dev'));
+// app.use('/uploads', express.static('uploads'));
+// app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(bodyParser.json());
 
-// CORS Headers
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', '*');
-    res.header('Access-Control-Allow-Credentials', true);
-    if (req.method === 'OPTIONS') {
-        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
-        return res.status(200).json({});
-    }
-    next();
-});
+// // CORS Headers
+// app.use((req, res, next) => {
+//     res.header('Access-Control-Allow-Origin', '*');
+//     res.header('Access-Control-Allow-Headers', '*');
+//     res.header('Access-Control-Allow-Credentials', true);
+//     if (req.method === 'OPTIONS') {
+//         res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+//         return res.status(200).json({});
+//     }
+//     next();
+// });
 
-// Routes
-app.use("/", Aave);
+// // Routes
+// app.use("/", Aave);
 
-// Default route
-app.get('/', (req, res) => {
-    res.send('Anant Gauranshi deployed it 🥳');
-});
+// // Default route
+// app.get('/', (req, res) => {
+//     res.send('Anant Gauranshi deployed it 🥳');
+// });
 
-// Error handling for 404
-app.use((req, res, next) => {
-    const error = new Error('Not found');
-    error.status = 404;
-    next(error);
-});
+// // Error handling for 404
+// app.use((req, res, next) => {
+//     const error = new Error('Not found');
+//     error.status = 404;
+//     next(error);
+// });
 
-// Error handling middleware
-app.use((error, req, res, next) => {
-    res.status(error.status || 500);
-    res.json({
-        error: {
-            message: error.message
-        }
-    });
-});
+// // Error handling middleware
+// app.use((error, req, res, next) => {
+//     res.status(error.status || 500);
+//     res.json({
+//         error: {
+//             message: error.message
+//         }
+//     });
+// });
 
-const PORT = process.env.PORT || 4000;
+// const PORT = process.env.PORT || 4000;
+
+// app.listen(PORT, () => {
+//     console.log(`Server is listening on port ${PORT}`);
+// });
+
+// module.exports = app;
+
+
+// index.js
+const express = require('express')
+
+const app = express()
+const PORT = 4000
 
 app.listen(PORT, () => {
-    console.log(`Server is listening on port ${PORT}`);
-});
+    console.log(`API listening on PORT ${PORT} `)
+})
 
-module.exports = app;
+app.get('/', (req, res) => {
+    res.send('Hey this is my API running 🥳')
+})
+
+app.get('/about', (req, res) => {
+    res.send('This is my about route..... ')
+})
+
+// Export the Express API
+module.exports = app
