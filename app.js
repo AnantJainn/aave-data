9626f4596fcd21ec017e9bb59f13ee17c0864545
Anant Jain <ritujainanant2810@gmail.com>
2023-08-18 14:09:59 +0530

    .

--- app.js
+++ app.js
@@ -54,7 +54,7 @@ app.use((error, req, res, next) => {
     });
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => {
     console.log(`Server is listening on port ${PORT}`);
