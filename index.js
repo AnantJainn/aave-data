28c137522a934c2d3c1cd103c102088166d68a1e
Anant Jain <ritujainanant2810@gmail.com>
2023-08-18 13:43:06 +0530

    .

--- index.js
+++ index.js
@@ -1,19 +1,50 @@
-const express = require('express')
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const Aave = require("./api/routes/aave.js");
+// import router from "./api/routes/aave.js";
+dotenv.config();
 
-const app = express()
-const PORT = 4000
+app.use(cors());
+app.use(morgan('dev'));
+app.use('/uploads', express.static('uploads'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-app.listen(PORT, () => {
-    console.log(`API listening on PORT ${PORT} `)
-})
+app.use(express.static('public'))
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', '*');
+    res.header('Access-Control-Allow-Credentials', true);
+    if (req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'PUT,POST,PATCH,DELETE,GET');
+        return res.status(200).json({});
+    }
+    next();
+});
 
+app.use("/", Aave);
 app.get('/', (req, res) => {
-    res.send('Hey this is my API running 🥳')
+    res.send('Anant Gauranshi deployed it 🥳')
+})
+app.use((req, res, next) => {
+    const error = new Error('Not found');
+    error.status = 404;
+    next(error);
 })
 
-app.get('/about', (req, res) => {
-    res.send('This is my about route..... ')
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
+    });
 })
 
-// Export the Express API
-module.exports = app
+app.listen(3000, () => {
+    console.log("server is listening on port 3000");
+})
